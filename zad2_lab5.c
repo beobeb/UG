c44983565cf668b03b59be9596e34a7e4ffe38f9
beobeb <114521210+beobeb@users.noreply.github.com>
2022-11-27 09:09:57 +0100

    zad2_lab5

---
+++ zad2_lab5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int x,y;
+
+    printf("Podaj dwie liczby: \n");
+    scanf("%d %d",&x,&y);
+
+    if(x==y)
+    printf("1");
+    else
+    printf("0");
+
+
+    return 0;
+}
